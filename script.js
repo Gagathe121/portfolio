a96761e056e26a352055f41cf5efe7bd76eb0b57
Gagathe121 <gagathecolors@gmail.com>
2025-03-22 15:02:11 +0100

    mise a jour footer et textes

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function toggleSection(sectionId) {
+// === Gestion du bouton "À propos" ===
+document.getElementById("toggle-apropos").addEventListener("click", function() {
+    const apropos = document.getElementById("apropos-content");
+      if (apropos.style.display === "" || apropos.style.display === "none") { // Vérifie l'état actuel et bascule entre "none" et "block"
+      apropos.style.display = "block";
+      apropos.scrollIntoView({ behavior: "smooth" }); // Défilement vers le bas de la page pour afficher le texte
+    } else {
+      apropos.style.display = "none"; // Masque le texte par défaut
+    } 
+  });
+
+  function toggleSection(sectionId) {
     const section = document.getElementById(sectionId);
     const isHidden = (section.style.display === "none" || section.style.display === "");
     
